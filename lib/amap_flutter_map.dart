24b983d47b2488a7c112077625ccb93ba11fa672
notfand <1528023125@qq.com>
2025-03-01 20:09:23 +0800

    0.0.4

--- lib/amap_flutter_map.dart
+++ lib/amap_flutter_map.dart
@@ -1,4 +1,6 @@
 /// 高德地图Flutter插件入口文件
 library amap_flutter_map;
 
-export 'package:amap_flutter_map/src/types/types.dart';
+export '/src/types/types.dart';
+export '/src/amap_controller.dart';
+export '/src/amap_widget.dart';
